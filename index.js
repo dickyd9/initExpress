b3be4f09f89521dfb0fc1a9c75b82ec73bfac996
dickyd9 <dickyd6@gmail.com>
2023-10-18 21:40:51 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express")
+const serverless = require("serverless-http")
+
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => {
+const router = express.Router()
+
+router.get("/", (req, res) => {
   res.send("Hello World!")
 })
+
+app.use("/api/", router)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = app;
+module.exports.handler = serverless(app)
